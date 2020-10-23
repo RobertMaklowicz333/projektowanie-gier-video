b679089071ea82bc6fdec27ffc24c1cbde69133e
RobertMaklowicz333 <justynaberlik77@gmail.com>
2020-10-23 11:28:25 +0200

    poprawki
    poprawki

--- godot01/Node.gd
+++ godot01/Node.gd
@@ -1,14 +1,14 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var sayHi = "Hello world"
+var welcomes = ["hi", "cześć", "priviet"]
+var names = ["Karen", "Justyna", "Sasza"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print (sayHi)
+	print (welcomes[1] + " nazywam się " + names[1])
+	print (welcomes[0] + " my name is " + names[0])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
