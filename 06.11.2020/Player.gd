c4d61c1f3c4a264b6e92f67746f247893c3af7dc
RobertMaklowicz333 <justynaberlik77@gmail.com>
2020-11-06 21:29:07 +0100

    Update Player.gd

--- 06.11.2020/Player.gd
+++ 06.11.2020/Player.gd
@@ -1,36 +1,41 @@
 extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 export var speed = 400
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+
+
 func _process(delta):
 	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"): 
 		velocity.x += 1
-	if Input.is_action_pressed("ui.left"):
+	if Input.is_action_pressed("ui_left"): 
 		velocity.x -= 1
-	if Input.is_action_pressed("ui.up"):
+	if Input.is_action_pressed("ui_up"): 
 		velocity.y -= 1
-	if Input.is_action_pressed("ui.down"):
+	if Input.is_action_pressed("ui_down"): 
 		velocity.y += 1
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * speed 
 		$AnimatedSprite.play()
-	else : 
+	else:
 		$AnimatedSprite.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
-	if velocity.x != 0: 
+
+	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip.h = velocity.x < 0
+		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
-		$AnimatedSprite.flip.v = velocity.y > 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		$AnimatedSprite.flip_v = velocity.y > 0
