4fc12251a995adca58851a4ed1d38444332d67ab
RobertMaklowicz333 <justynaberlik77@gmail.com>
2020-11-19 17:04:34 +0100

    v.3
    -została dodana scena bullet która wgl odpowiada tutaj za instancję pocisku
    -dopisana zmienna bullet i bullet speed i funkcja odpowiadająca za strzelanie i wywołuje one poprzednio opisaną scenę
    -prawdą jest że znaczną część tego fragmentu kodu wzięłam z internetu, ale po wyjaśnieniu w poradniku nawet ją rozumiem

--- 13.11.2020 (projekt)/player.gd
+++ 13.11.2020 (projekt)/player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 var movespeed = 500
-
+var bullet_speed = 2000
+var bullet = preload("res://Bullet.tscn") #czyli najpierw zrobiłam scenę z tym pociskiem, teraz wykorzystuję w kodzie na bazie sugestii z poradnika
+#teraz trzeba napisać funkcję fire i znajdzie się ona pod kodem poruszania się
 
 func _ready():
 	pass # Replace with function body.
@@ -21,3 +23,13 @@ func _physics_process(delta): #polecane w jednym z poradników, z których korzy
 	motion = move_and_slide(motion * movespeed)
 	
 	look_at(get_global_mouse_position()) #nie znałam tego wcześniej, ale w jednym z poradników ktoś to polecił i wytłumaczył (chodzi o to, żeby można było mierzyć tam, gdzie znajduje się myszka)
+	
+	if Input.is_action_just_pressed("LPM"):
+		fire()
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_mouse_position() #na to oczywiście sama nie wpadłam, ale po przeczytaniu o tym ma to oczywiście sens
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deffered("add_child",bullet_instance)
