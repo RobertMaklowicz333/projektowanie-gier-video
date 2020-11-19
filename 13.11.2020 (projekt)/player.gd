c20ea20d83b3dd398ec0b974aaf057d6ea3cdbbe
RobertMaklowicz333 <justynaberlik77@gmail.com>
2020-11-19 16:43:06 +0100

    c: v.2
    oh, tutaj są te zmiany, o których pisałam (zapomniałam zapisać scenę wcześniej)

--- 13.11.2020 (projekt)/player.gd
+++ 13.11.2020 (projekt)/player.gd
@@ -1,16 +1,23 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var movespeed = 500
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta): #polecane w jednym z poradników, z których korzystam
+	var motion = Vector2()
+	if Input.is_action_pressed("up"):
+		motion.y -= 1
+	if Input.is_action_pressed("down"):
+		motion.y += 1
+	if Input.is_action_pressed("right"):
+		motion.x += 1
+	if Input.is_action_pressed("left"):
+		motion.x -= 1
+	#to wszystko co powyżej, to zupełnie jak na zajęciach, to co poniżej zaś to fuzja z paru poradników)
+	motion = motion.normalized()
+	motion = move_and_slide(motion * movespeed)
+	
+	look_at(get_global_mouse_position()) #nie znałam tego wcześniej, ale w jednym z poradników ktoś to polecił i wytłumaczył (chodzi o to, żeby można było mierzyć tam, gdzie znajduje się myszka)
