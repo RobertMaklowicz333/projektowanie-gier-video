2c3a8034b9b5828b1741dca959494339f96f4f13
RobertMaklowicz333 <justynaberlik77@gmail.com>
2021-01-21 21:24:17 +0100

    .
    .

--- c - 22.01.2021 (Guess the number)/playagain.gd
+++ c - 22.01.2021 (Guess the number)/playagain.gd
@@ -1,12 +1,5 @@
 extends Button
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
@@ -16,5 +9,6 @@ func _ready():
 #	pass
 
 
-func _on_playagain_pressed(button_pressed):
+
+func _on_playagain_pressed():
 	get_tree().change_scene("res://1.tscn")
